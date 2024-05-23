6ac2a2314353f561c2b2c99196bc1fb2f56f6b0f
matthew-upshaw <matthew.upshaw02@gmail.com>
2024-05-23 08:50:03 -0500

    Reorganize methods in fem_analysis.py

--- pondpy/analysis/fem_analysis.py
+++ pondpy/analysis/fem_analysis.py
@@ -46,180 +46,6 @@ class BeamModel:
     if ini_analysis:
       self.initialize_analysis()
 
-  def _get_points_of_interest(self):
-    '''
-    Defines points of interest along the length of the beam model,
-    including beam start and end points, points of load application, and
-    support points.
-    '''
-    points_of_interest = []
-
-    # Add end points
-    points_of_interest.extend([0, self.beam.length])
-
-    # Add support points
-    for support in self.beam.supports:
-      if support[0] not in points_of_interest:
-        points_of_interest.append(support[0])
-
-    # Add locations of point loads
-    for load in self.beam.ploads:
-      if load.location not in points_of_interest:
-        points_of_interest.append(load.location)
-
-    # Add starting and ending locations of distributed loads
-    for load in self.beam.dloads:
-      for point in load.location:
-        if point not in points_of_interest:
-          points_of_interest.append(point)
-
-    points_of_interest.sort()
-
-    self.points_of_interest = points_of_interest
-
-  def _create_model_nodes_and_elems(self):
-    '''
-    Defines the model nodes based on the points of interest
-    and maximum node spacing defined by the user.
-    '''
-    model_segments = []
-    for idx, poi in enumerate(self.points_of_interest[:-1]):
-      segment = (self.points_of_interest[idx], self.points_of_interest[idx+1])
-
-      model_segments.append(segment)
-
-    nodes = []
-    for pair in model_segments:
-      # Determine number of sub-segments
-      n_sub = math.ceil((pair[1]-pair[0])/self.max_node_spacing)
-      # Subdivide segments and add the nodes to the model_nodes list
-      nodes.extend([((pair[1]-pair[0])/n_sub)*i+pair[0] for i in range(n_sub+1)])
-
-    # Remove duplicate nodes
-    model_nodes = []
-    for node in nodes:
-      if node not in model_nodes:
-        model_nodes.append(node)
-
-    # Store end nodes for each element
-    elem_nodes = []
-    for idx, node in enumerate(model_nodes[:-1]):
-      elem_nodes.append([idx, idx+1])
-
-    self.model_nodes = model_nodes
-    self.elem_nodes = elem_nodes
-
-  def _set_support_nodes(self):
-    '''
-    Determines which model nodes have been defined by the
-    user as supports.
-    '''
-    node_support = [(0, 0, 0) for _ in range(len(self.model_nodes))]
-    support_nodes = []
-    for i_node, node in enumerate(self.model_nodes):
-      for support in self.beam.supports:
-        if support[0] == node:
-          node_support[i_node] = support[1]
-          support_nodes.append(i_node)
-
-    self.node_support = node_support
-    self.support_nodes = support_nodes
-
-  def _set_pload_nodes(self):
-    '''
-    Determines which model nodes have been defined by the
-    user as having applied point loads.
-    '''
-    node_pload = [[0, 0, 0] for _ in range(len(self.model_nodes))]
-    for idx, node in enumerate(self.model_nodes):
-      for pload in self.beam.ploads:
-        if pload.location == node:
-          for idx2, _ in enumerate(node_pload[idx]):
-            node_pload[idx][idx2] += pload.magnitude[idx2]
-
-    self.node_pload = node_pload
-
-  def _set_dload_elems(self):
-    '''
-    Determines which model elements have been defined by the user
-    as having applied distributed loads.
-    '''
-    elem_dload  = [[[0, 0], [0,0], [0,0]] for _ in range(len(self.elem_nodes))]
-    for idx, elem in enumerate(self.elem_nodes):
-      for dload in self.beam.dloads:
-        m = []
-        b = []
-        for dir in dload.magnitude:
-          m.append((dir[1]-dir[0])/(dload.location[1]-dload.location[0]))
-          b.append(dir[0])
-        if (dload.location[0] <= self.model_nodes[elem[0]] and
-            dload.location[1] >= self.model_nodes[elem[1]]):
-          for idx2, _ in enumerate(elem_dload[idx]):
-            elem_dload[idx][idx2][0] = m[idx2]*self.model_nodes[elem[0]]+b[idx2]
-            elem_dload[idx][idx2][1] = m[idx2]*self.model_nodes[elem[1]]+b[idx2]
-
-    self.elem_dload = elem_dload
-
-  def _get_node_elem_fef(self):
-    '''
-    Calculates the fixed end forces due to distributed loads on
-    model elements.
-    '''
-    node_elem_fef = [[0, 0 ,0] for _ in range(len(self.model_nodes))]
-    # Loop over all elements
-    for idx, elem in enumerate(self.elem_nodes):
-      # Calculate fixed end forces for y-distributed loads
-      w1 = self.elem_dload[idx][1][0]
-      w2 = self.elem_dload[idx][1][1]
-      L = self.model_nodes[elem[1]] - self.model_nodes[elem[0]]
-      if w1 != 0 and w2 != 0:
-        # Calculate fixed end forces if w1 >= w2
-        if abs(w1) > abs(w2):
-          v_react_i = (w2*L)/2 + (7*(w1-w2)*L)/20
-          v_react_j = (w2*L)/2 + (3*(w1-w2)*L)/20
-          m_react_i = (w2*L**2)/12 + ((w1-w2)*L**2)/20
-          m_react_j = -(w2*L**2)/12 - ((w1-w2)*L**2)/30
-        # Calculate fixed end forces if w1 <= w2
-        elif abs(w2) >= abs(w1):
-          v_react_i = (w1*L)/2 + (3*(w2-w1)*L)/20
-          v_react_j = (w1*L)/2 + (7*(w2-w1)*L)/20
-          m_react_i = (w1*L**2)/12 + ((w2-w1)*L**2)/30
-          m_react_j = -(w1*L**2)/12 - ((w2-w1)*L**2)/20
-        # Place the fixed end forces in the proper location
-        node_elem_fef[elem[0]][1] += v_react_i
-        node_elem_fef[elem[0]][2] += m_react_i
-        node_elem_fef[elem[1]][1] += v_react_j
-        node_elem_fef[elem[1]][2] += m_react_j
-
-    self.node_elem_fef = node_elem_fef
-
-  def _fill_global_dof(self):
-    '''
-    Fills global dof array with the appropriate global dof number
-    and determine the total number of dof in the model.
-    '''
-    # Initialize list with n_nodes rows and 3 columns in each row
-    dof_num = [[0, 0, 0] for _ in range(len(self.model_nodes))]
-
-    # Initialize the dof counter
-    dof_count = 0
-
-    # Loop over all nodes
-    for i_node in range(len(self.model_nodes)):
-      # Loop over each dof
-      for i_dof in range(3):
-        # Increase total dof number for each new dof
-        if self.node_support[i_node][i_dof] == 0:
-          # This node is not restrained and is a dof
-          dof_count += 1
-          dof_num[i_node][i_dof] = dof_count
-        else:
-          # This node is restrained
-          dof_num[i_node][i_dof] = 0
-
-    self.dof_num = dof_num
-    self.n_dof = dof_count
-
   def _assemble_global_stiffness(self):
     '''
     Assembles the global stiffness matrix for the model.
@@ -287,6 +113,65 @@ class BeamModel:
     self.global_stiffness = S
     self.local_stiffness_matrices = local_stiffness_matrices
 
+  def _create_model_nodes_and_elems(self):
+    '''
+    Defines the model nodes based on the points of interest
+    and maximum node spacing defined by the user.
+    '''
+    model_segments = []
+    for idx, poi in enumerate(self.points_of_interest[:-1]):
+      segment = (self.points_of_interest[idx], self.points_of_interest[idx+1])
+
+      model_segments.append(segment)
+
+    nodes = []
+    for pair in model_segments:
+      # Determine number of sub-segments
+      n_sub = math.ceil((pair[1]-pair[0])/self.max_node_spacing)
+      # Subdivide segments and add the nodes to the model_nodes list
+      nodes.extend([((pair[1]-pair[0])/n_sub)*i+pair[0] for i in range(n_sub+1)])
+
+    # Remove duplicate nodes
+    model_nodes = []
+    for node in nodes:
+      if node not in model_nodes:
+        model_nodes.append(node)
+
+    # Store end nodes for each element
+    elem_nodes = []
+    for idx, node in enumerate(model_nodes[:-1]):
+      elem_nodes.append([idx, idx+1])
+
+    self.model_nodes = model_nodes
+    self.elem_nodes = elem_nodes
+
+  def _fill_global_dof(self):
+    '''
+    Fills global dof array with the appropriate global dof number
+    and determine the total number of dof in the model.
+    '''
+    # Initialize list with n_nodes rows and 3 columns in each row
+    dof_num = [[0, 0, 0] for _ in range(len(self.model_nodes))]
+
+    # Initialize the dof counter
+    dof_count = 0
+
+    # Loop over all nodes
+    for i_node in range(len(self.model_nodes)):
+      # Loop over each dof
+      for i_dof in range(3):
+        # Increase total dof number for each new dof
+        if self.node_support[i_node][i_dof] == 0:
+          # This node is not restrained and is a dof
+          dof_count += 1
+          dof_num[i_node][i_dof] = dof_count
+        else:
+          # This node is restrained
+          dof_num[i_node][i_dof] = 0
+
+    self.dof_num = dof_num
+    self.n_dof = dof_count
+
   def _get_load_vector(self):
     '''
     Assembles the global load vector, including fixed end forces
@@ -307,6 +192,147 @@ class BeamModel:
 
     self.nodal_load_vector = P
     self.fef_load_vector = Pf
+
+  def _get_node_elem_fef(self):
+    '''
+    Calculates the fixed end forces due to distributed loads on
+    model elements.
+    '''
+    node_elem_fef = [[0, 0 ,0] for _ in range(len(self.model_nodes))]
+    # Loop over all elements
+    for idx, elem in enumerate(self.elem_nodes):
+      # Calculate fixed end forces for y-distributed loads
+      w1 = self.elem_dload[idx][1][0]
+      w2 = self.elem_dload[idx][1][1]
+      L = self.model_nodes[elem[1]] - self.model_nodes[elem[0]]
+      if w1 != 0 and w2 != 0:
+        # Calculate fixed end forces if w1 >= w2
+        if abs(w1) > abs(w2):
+          v_react_i = (w2*L)/2 + (7*(w1-w2)*L)/20
+          v_react_j = (w2*L)/2 + (3*(w1-w2)*L)/20
+          m_react_i = (w2*L**2)/12 + ((w1-w2)*L**2)/20
+          m_react_j = -(w2*L**2)/12 - ((w1-w2)*L**2)/30
+        # Calculate fixed end forces if w1 <= w2
+        elif abs(w2) >= abs(w1):
+          v_react_i = (w1*L)/2 + (3*(w2-w1)*L)/20
+          v_react_j = (w1*L)/2 + (7*(w2-w1)*L)/20
+          m_react_i = (w1*L**2)/12 + ((w2-w1)*L**2)/30
+          m_react_j = -(w1*L**2)/12 - ((w2-w1)*L**2)/20
+        # Place the fixed end forces in the proper location
+        node_elem_fef[elem[0]][1] += v_react_i
+        node_elem_fef[elem[0]][2] += m_react_i
+        node_elem_fef[elem[1]][1] += v_react_j
+        node_elem_fef[elem[1]][2] += m_react_j
+
+    self.node_elem_fef = node_elem_fef
+
+  def _get_points_of_interest(self):
+    '''
+    Defines points of interest along the length of the beam model,
+    including beam start and end points, points of load application, and
+    support points.
+    '''
+    points_of_interest = []
+
+    # Add end points
+    points_of_interest.extend([0, self.beam.length])
+
+    # Add support points
+    for support in self.beam.supports:
+      if support[0] not in points_of_interest:
+        points_of_interest.append(support[0])
+
+    # Add locations of point loads
+    for load in self.beam.ploads:
+      if load.location not in points_of_interest:
+        points_of_interest.append(load.location)
+
+    # Add starting and ending locations of distributed loads
+    for load in self.beam.dloads:
+      for point in load.location:
+        if point not in points_of_interest:
+          points_of_interest.append(point)
+
+    points_of_interest.sort()
+
+    self.points_of_interest = points_of_interest
+
+  def _set_dload_elems(self):
+    '''
+    Determines which model elements have been defined by the user
+    as having applied distributed loads.
+    '''
+    elem_dload  = [[[0, 0], [0,0], [0,0]] for _ in range(len(self.elem_nodes))]
+    for idx, elem in enumerate(self.elem_nodes):
+      for dload in self.beam.dloads:
+        m = []
+        b = []
+        for dir in dload.magnitude:
+          m.append((dir[1]-dir[0])/(dload.location[1]-dload.location[0]))
+          b.append(dir[0])
+        if (dload.location[0] <= self.model_nodes[elem[0]] and
+            dload.location[1] >= self.model_nodes[elem[1]]):
+          for idx2, _ in enumerate(elem_dload[idx]):
+            elem_dload[idx][idx2][0] = m[idx2]*self.model_nodes[elem[0]]+b[idx2]
+            elem_dload[idx][idx2][1] = m[idx2]*self.model_nodes[elem[1]]+b[idx2]
+
+    self.elem_dload = elem_dload
+
+  def _set_pload_nodes(self):
+    '''
+    Determines which model nodes have been defined by the
+    user as having applied point loads.
+    '''
+    node_pload = [[0, 0, 0] for _ in range(len(self.model_nodes))]
+    for idx, node in enumerate(self.model_nodes):
+      for pload in self.beam.ploads:
+        if pload.location == node:
+          for idx2, _ in enumerate(node_pload[idx]):
+            node_pload[idx][idx2] += pload.magnitude[idx2]
+
+    self.node_pload = node_pload
+
+  def _set_support_nodes(self):
+    '''
+    Determines which model nodes have been defined by the
+    user as supports.
+    '''
+    node_support = [(0, 0, 0) for _ in range(len(self.model_nodes))]
+    support_nodes = []
+    for i_node, node in enumerate(self.model_nodes):
+      for support in self.beam.supports:
+        if support[0] == node:
+          node_support[i_node] = support[1]
+          support_nodes.append(i_node)
+
+    self.node_support = node_support
+    self.support_nodes = support_nodes
+
+  def add_beam_dload(self, dload, add_type='add'):
+    '''
+    Adds a distributed load to the Beam object referenced by the BeamModel object.
+    '''
+    # Add the distributed load
+    if add_type == 'replace':
+      self.beam.dloads = dload
+    elif add_type == 'add':
+      self.beam.dloads.extend(dload)
+
+    # Re-initialize the analysis
+    self.initialize_analysis()
+
+  def add_beam_pload(self, pload, add_type='add'):
+    '''
+    Adds a point load to the Beam object referenced by the BeamModel object.
+    '''
+    # Add the point load
+    if add_type == 'replace':
+      self.beam.ploads = pload
+    elif add_type == 'add':
+      self.beam.ploads.extend(pload)
+
+    # Re-initialize the analysis
+    self.initialize_analysis()
 
   def initialize_analysis(self):
     '''
@@ -405,32 +431,6 @@ class BeamModel:
     plt.grid()
 
     plt.show()
-
-  def add_beam_dload(self, dload, add_type='add'):
-    '''
-    Adds a distributed load to the Beam object referenced by the BeamModel object.
-    '''
-    # Add the distributed load
-    if add_type == 'replace':
-      self.beam.dloads = dload
-    elif add_type == 'add':
-      self.beam.dloads.extend(dload)
-
-    # Re-initialize the analysis
-    self.initialize_analysis()
-
-def add_beam_pload(self, pload, add_type='add'):
-    '''
-    Adds a point load to the Beam object referenced by the BeamModel object.
-    '''
-    # Add the point load
-    if add_type == 'replace':
-      self.beam.ploads = pload
-    elif add_type == 'add':
-      self.beam.ploads.extend(pload)
-
-    # Re-initialize the analysis
-    self.initialize_analysis()
 
 class DistLoad:
   def __init__(self, location, magnitude):
