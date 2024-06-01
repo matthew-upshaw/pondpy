3ef95aaa61aeb7987292e7ca43422c8c639a7187
matthew-upshaw <91103287+matthew-upshaw@users.noreply.github.com>
2024-06-01 12:23:00 -0500

    Update tests for DistLoad class

--- tests/test_dist_load.py
+++ tests/test_dist_load.py
@@ -13,10 +13,35 @@ def test_dload_initialization(valid_dload):
     assert valid_dload.location == location
     assert valid_dload.magnitude == magnitude
 
-def test_invalid_location():
+def test_invalid_location_type():
     with pytest.raises(TypeError):
-        DistLoad(location=(None, 0.0), magnitude=magnitude)
+        DistLoad(location=None, magnitude=magnitude)
+        
+def test_invalid_location_length():
+    with pytest.raises(TypeError):
+        DistLoad(location=(0), magnitude=magnitude)
+        
+def test_invalid_location_component():
+    with pytest.raises(TypeError):
+        DistLoad(location=('0', '10'), magnitude=magnitude)
 
-def test_invalid_magnitude():
+def test_invalid_magnitude_type():
     with pytest.raises(TypeError):
-        DistLoad(location=location, magnitude=((0.0, None), (0, 0, 0), (None, None)))
+        DistLoad(location=location, magnitude=None)
+        
+def test_invalid_magnitude_length():
+    with pytest.raises(TypeError):
+        DistLoad(location=location, magnitude=((0, 0, 0), (0, 0, 0)))
+        
+def test_invalid_magnitude_component_type():
+    with pytest.raises(TypeError):
+        DistLoad(location=location, magnitude=(None, None, None))
+        
+def test_invalid_magnitude_component_length():
+    with pytest.raises(TypeError):
+        DistLoad(location=location, magnitude=((5), (5), (5)))
+        
+def test_invalid_magnitude_inner_component():
+    with pytest.raises(TypeError):
+        DistLoad(location=location, magnitude=(('0', '0', '0'), ('0', '0', '0'), ('0', '0', '0')))
+        
